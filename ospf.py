72170902539c35d4ec278fdea6305e6e8fac8790
Mustafa <mceylan6634@gmail.com>
2022-03-13 23:07:25 +0300

    OSPF

---
+++ ospf.py
@@ -0,0 +1,10 @@
+import streamlit as st
+# import paramiko
+
+st.title('Sekuritim OSPF Anons Software')
+
+networkAddr = st.text_input('Anons edilmesini istediğiniz network adresini yazınız')
+networkComment = st.text_input('Açıklama yazınız (Örn: Kurumsal)')
+st.write('OSPF Network Address : ', networkAddr)
+st.write('OSPF Network Comment : ', networkComment)
+
